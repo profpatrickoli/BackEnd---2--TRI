03965773aa1bd6ced6788193ae7fd23bd33156dc
profpatrickoli <patrick.staroin@escola.pr.gov>
2024-05-21 08:54:42 -0300

    Resolução dos exercícios

--- Aula2-Objetos.js
+++ Aula2-Objetos.js
@@ -11,15 +11,34 @@ lanches = [];
 lanches.push(criaLanche("x-burguer", 10.0, ["pão", "hamburguer", "queijo"]));
 lanches.push(criaLanche("x-salada", 12.0, ["pão", "hamburguer", "queijo", "salada"]));
 lanches.push(criaLanche("x-ratazana", 30.0, ["pão", "hamburguer", "queijo", "ratazana"]));
-console.log(lanches);
+
 
 /* Para essa aula:
 1 - Criar uma função para alterar o preço do lanche.
 2 - Criar uma função para alterar os ingredientes do lanche.
-3 - Criar uma função para mostrar o lanche.
+3 - Criar uma função para mostrar todos os lanche.
 */
 function alteraPreco(lanche, novoPreco) {
     lanche.preco = novoPreco;
+    console.log("Preço alterado para " + lanche.preco);
 }
 alteraPreco(lanches[0], 20.0);
-console.log(lanches[0]);
+
+function removeIngrediente(lanche){
+    lanche.ingredientes.pop();
+    console.log("Novos ingredientes: " + lanche.ingredientes);
+}
+removeIngrediente(lanches[0]); 
+
+function adicionaIngrediente(lanche, ingrediente){
+    lanche.ingredientes.push(ingrediente);
+    console.log("Novos ingredientes: " + lanche.ingredientes);
+}
+adicionaIngrediente(lanches[0], "Hamburguer artesanal");
+  
+function mostraLanches(){
+    lanches.forEach((lanche) => {
+        console.log(lanche.nome + " - R$ " + lanche.preco + " - " + lanche.ingredientes);
+    });
+}
+mostraLanches();
